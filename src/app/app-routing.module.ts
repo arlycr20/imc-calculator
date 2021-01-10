bda8f006f4740111e1c0c65a61bf777f087dacee
gustavR0 <gust040825@gmail.com>
2021-01-09 18:47:57 -0600

    add login #28

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { UiComponent } from './ui/ui.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
-  {path:'', component: UiComponent},
-  {path:'ui', component: UiComponent},
+  {path:'', component: UiComponent, canActivate: [ AuthGuard ]},
+  {path:'ui', component: UiComponent, canActivate: [ AuthGuard ]},
+  {path:'login', component: LoginComponent},
 ];
 
 @NgModule({
