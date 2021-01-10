6d24e98d1bd5738a37db0fcd93fc94fe02ed3a11
arlycr20 <arelycr07@hotmail.com>
2021-01-09 23:12:39 -0600

    routing signin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { UiComponent } from './ui/ui.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth.guard';
+import { SigninComponent } from './signin/signin.component';
 
 
 const routes: Routes = [
   {path:'', component: UiComponent, canActivate: [ AuthGuard ]},
   {path:'ui', component: UiComponent, canActivate: [ AuthGuard ]},
   {path:'login', component: LoginComponent},
+  {path:'signin', component: SigninComponent}
 ];
 
 @NgModule({
